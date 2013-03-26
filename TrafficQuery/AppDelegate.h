2e7a94e95b94694780c6752cd7cf223178e0daca
hz <tanpeng@bogon.(none)>
2013-03-26 09:11:15 +0800

    Initial Commit

---
+++ TrafficQuery/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrafficQuery
+//
+//  Created by hz on 13-3-26.
+//  Copyright (c) 2013年 hz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
